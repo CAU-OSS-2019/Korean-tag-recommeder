f51d11c8a6dabdf3a517100017185c3648cafa58
nh7881 <nh7881@naver.com>
2019-05-19 02:46:42 +0900

    bin파일로 저장하는거 추가

--- Dataset1/keyword/keyword_recomend_by_data1-2.py
+++ Dataset1/keyword/keyword_recomend_by_data1-2.py
@@ -43,10 +43,10 @@ def search_key(count_list, freq_data, loop):
 
 def main():
     
-    with open('./opensource/Data_1-1.bin', 'rb') as f:
+    with open('./opensource/0.2million/Data_1-1.bin', 'rb') as f:
         letter_data = pickle.load(f)
         
-    with open('./opensource/Data_1-2.bin', 'rb') as f2:
+    with open('./opensource/0.2million/Data_1-2.bin', 'rb') as f2:
         freq_data = pickle.load(f2)
 
     keyword = []
@@ -55,6 +55,23 @@ def main():
     for i in range(0, len(letter_data)) :
         count_list = Counter(letter_data[i])
         keyword.append(search_key(count_list, freq_data, loop))
-    print(keyword)
+
+    # 결과 리스트 저장
+    save_dir = 'keyword.bin'  # 저장 경로
+    with open(save_dir, 'wb') as fwrite:
+        pickle.dump(keyword, fwrite)
+
+    fwrite.close()
+
+
+    # 전처리한 데이터 불러와서 잘 저장되었는지 확인
+    with open(save_dir, 'rb') as f:
+        data = pickle.load(f)  # 한 줄씩 읽어옴
+
+    for i in range(5):  # 5줄만 테스트로 읽어옴
+        print(data[i])
+
+    f.close()
+    
     
 main()
