dde218f3688d19cad47c2a88b0111d8d158bfe1b
EJ <dmswn5829@gmail.com>
2019-05-21 16:02:49 +0900

    Add workingtime

--- Dataset1/kowiki/kowiki_data_tokenizing.py
+++ Dataset1/kowiki/kowiki_data_tokenizing.py
@@ -1,37 +1,21 @@
 '''
 위키피디아 한국어 파일을 tokenizing하여 저장.
-코드 구동 전 아래 명령어를 먼저 실행해야 함
-
-    copy 현재 디렉토리의 경로\wikiA* wiki_data.txt
-
-
-자세한 설명은 아래 페이지를 참고
-https://github.com/CAU-OSS-2019/team-project-team18/wiki/위키피디아-한국어-덤프파일을-이용한-word2vec-데이터셋-제작(명사토큰화까지)
-
-*주의*  
-코드 실행시 4시간 이상의 시간이 소요되므로 시간적 여유가 있을 때 돌려야 함 
-화면이 꺼지거나 절전모드 전환 시 중단될 우려가 있음!  
-(전체 6317871 lines)  
+40분 정도 소요됨.
 
 * 코드 실험을 위해서는 test_kowiki_data_tokenizing.py 구동
+
 '''
 
-
-# 데이터파일 잘 읽어와지는지 확인
-import sys
 from konlpy.tag import Okt
 import pickle
+import time
+
 
 def main():
-
+    
+    # 데이터파일 잘 읽어와지는지 확인
     data_dir = 'wikiAA.txt'  # 파일 경로
-    try:
-        ftest = open(data_dir, 'r', encoding="utf-8")
-    except FileNotFoundError as e:
-        print(e)
-        print("Run the following command first\n" +
-            "    copy .\\wikiA* wiki_data.txt")
-        sys.exit()
+    ftest = open(data_dir, 'r', encoding="utf-8")
 
     i = 0
     while True:
@@ -43,7 +27,6 @@ def main():
             break 
     ftest.close()
 
-
     # Tokenizing
     okt = Okt()
 
@@ -52,6 +35,7 @@ def main():
 
     n = 0
     result = []
+    start_time = time.time()
 
     while True:
         line = fread.readline()  # 한 줄씩 읽음
@@ -61,10 +45,10 @@ def main():
         if n % 5000 == 0:  # 5,000의 배수로 While문이 실행될 때마다 몇 번째 실행인지 출력
             print("%d번째 While문."%n)
 
-        tokenlist = okt.nouns(line) # 단어 토큰화, 명사만 리스트에 넣음
+        tokenlist = okt.nouns(line)  # 단어 토큰화, 명사만 리스트에 넣음
         
-        with open('stopwords.txt','rt', encoding='UTF8') as f:
-            b=f.read().split()
+        with open('stopwords.txt', 'rt', encoding='UTF8') as f:
+            b = f.read().split()
             for i in b:
                 if i in tokenlist:
                     tokenlist.remove(i)
@@ -73,14 +57,15 @@ def main():
             result.append(tokenlist)  # 결과에 저장
     fread.close()
 
-
     # 결과 리스트 저장
     save_dir = 'wiki_tokened.bin'  # 저장 경로
     with open(save_dir, 'wb') as fwrite:
         pickle.dump(result, fwrite)
-
     fwrite.close()
 
+    end_time = time.time()
+    print("\n", len(result))
+    print("WorkingTime: {0:0.2f} sec\n".format(end_time-start_time))
 
     # 전처리한 데이터 불러와서 잘 저장되었는지 확인
     with open(save_dir, 'rb') as f:
@@ -90,6 +75,7 @@ def main():
         print(data[i])
 
     f.close()
+
 
 if __name__ == '__main__':
     main()
--- Dataset1/kowiki/test_kowiki_data_tokenizing.py
+++ Dataset1/kowiki/test_kowiki_data_tokenizing.py
@@ -1,21 +1,17 @@
 '''
 * test code *
+It takes less than a minute.
 
-위키피디아 한국어 파일을 tokenizing하여 저장.
-코드 구동 전 아래 명령어를 먼저 실행해야 함
-
-자세한 설명은 아래 페이지를 참고
-https://github.com/CAU-OSS-2019/team-project-team18/wiki/위키피디아-한국어-덤프파일을-이용한-word2vec-데이터셋-제작(명사토큰화까지)
 '''
 
-
-# 데이터파일 잘 읽어와지는지 확인
-import sys
 from konlpy.tag import Okt
 import pickle
+import time
+
 
 def main():
 
+    # 데이터파일 잘 읽어와지는지 확인
     data_dir = 'wikiAA.txt'  # 파일 경로
     ftest = open(data_dir, 'r', encoding="utf-8")
 
@@ -39,10 +35,11 @@ def main():
     #n = 0
     test_num = 10000 # 테스트할 line 수
     result = []
+    start_time = time.time()
 
     for n in range(test_num):
         line = fread.readline()  # 한 줄씩 읽음
-        if not line: break # 모두 읽으면 while문 종료
+        if not line: break # 모두 읽으면 for문 종료
         
         n = n + 1
         if n % 1000 == 0:  # 1,000의 배수로 While문이 실행될 때마다 몇 번째 실행인지 출력
@@ -60,14 +57,15 @@ def main():
             result.append(tokenlist)  # 결과에 저장
     fread.close()
 
-
     # 결과 리스트 저장
     save_dir = 'test_wiki_tokened.bin'  # 저장 경로
     with open(save_dir, 'wb') as fwrite:
         pickle.dump(result, fwrite)
-
     fwrite.close()
 
+    end_time = time.time()
+    print("\n", len(result))
+    print("WorkingTime: {0:0.2f} sec\n".format(end_time-start_time))
 
     # 전처리한 데이터 불러와서 잘 저장되었는지 확인
     with open(save_dir, 'rb') as f:
@@ -77,6 +75,7 @@ def main():
         print(data[i])
 
     f.close()
+
 
 if __name__ == '__main__':
     main()
